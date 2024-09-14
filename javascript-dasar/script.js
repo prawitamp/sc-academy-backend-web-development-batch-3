9d6a4ce384d9e0f6d660422a5f95faaa1750a34d
Prawita Mepilianti <prawitamepilianti@gmail.com>
2024-09-14 15:50:42 +0700

    first commit

---
+++ javascript-dasar/script.js
@@ -0,0 +1,46 @@
+// // variable
+
+// // variable dengan tipe data integer
+// const variableSatu = 1;
+// console.log(variableSatu);
+
+// // variable dengan tipe data string
+// const variableDua = "isi variable";
+// console.log(variableDua);
+
+// // variable dengan tipe data desimal
+// const variableTiga = 3.14;
+// console.log(variableTiga);
+
+// // variable dengan tipe data array
+// const variableEmpat = [1, 2, 3, 4];
+// console.log(variableEmpat);
+
+// // variable dengan tipe data object
+// const variableLima = { nama: "John Doe", umur: 30 };
+// console.log(variableLima);
+
+// // variable dengan tipe data boolean
+// const variableEnam = true;
+// console.log(variableEnam);
+
+// function
+let data1 = false;
+let data2 = false;
+
+function coba(data1, data2) {
+  // eksekusi kode disini
+  //   console.log(data1);
+  //   console.log(data2);
+  if (data1 === true) {
+    data1 = "berhasil login";
+  } else {
+    data1 = "gagal login";
+  }
+  console.log(data1);
+}
+coba(data1, data2); // argument
+
+// perkalian
+console.log(5 * 5);
+console.log(5 + 5);
